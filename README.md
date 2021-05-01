21547f8e37cb0b94bf803c7592090d09e9e21c4d
German Eksi <german.eksi@gmail.com>
2021-05-02 00:36:39 +0300

    added sunrise and sunset API for data

---
+++ README.md
@@ -0,0 +1,9 @@
+Tere tulemast päeva pikkuse arvutamise rakenduse reposse!! 😜
+
+#Rakendus on loodud CGI suvepraktikale saamise eesmärgil.
+#Eesmärgiks on luua rakendus, mis arvutab päeva pikkust, kasutades pikkus ja -laiuskraade ning kuupäeva parameetritena.
+
+Tähtsad punktid olid:
+
+1. Päeva pikkuse arvutamisel tuleb leida päikesetõusu ja -loojangu vaheline aeg.
+2. Rakenduse kasutaja peab saama asukohta määrata sisestades koordinaate käsitsi või siis valides asukoha kaardil.
--- index.js
+++ index.js
@@ -1,15 +1,60 @@
 //loading in the map
 
-// Copied from the leafletjs original documentation page
-// Rreference: https://leafletjs.com/index.html
-var map = L.map("map").setView([51.505, -0.09], 13);
+//Global variables to use
+let latitude = document.getElementById("lat").value;
+let longitude = document.getElementById("long").value;
+let date = document.getElementById("date").value;
+const zoomLevel = 13;
+let map = L.map("map");
+let coords = [latitude, longitude];
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
+// Fetch sunrise, sunset and day length data from external URL.
+// For the url we need longitude, latitude. (date is optional but we will include it, so we could fetch data based on given data)
+/*$(document).ready(function () {
+  let url = `https://api.sunrise-sunset.org/json?lat=${58.35833}&lng=${26.71326}&date=${today}`;
+});*/
 
-L.marker([51.5, -0.09])
-  .addTo(map)
-  .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-  .openPopup();
+$.ajax({
+  url: `https://api.sunrise-sunset.org/json?lat=${58.35833}&lng=${26.71326}&date=${"today"}`,
+  contentType: "application/json",
+  dataType: "jsonp",
+  success: function (data) {
+    console.log(data);
+  },
+});
+
+//Function for loading the map
+function loadMap() {
+  map.setView([51.505, -0.09], 13);
+
+  //Changed the titleLayer so it would look a bit different in style
+  L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+}
+
+//Function for loading the position
+function loadPosition() {
+  // Copied from the leafletjs original documentation page
+  // Reference: https://leafletjs.com/index.html
+  L.marker([58.35833, 26.71326])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidht: 250,
+        maxHeight: 150,
+        className: "leaflet-popup",
+      })
+    )
+    .setPopupContent(
+      `
+     Day Length: ${"wdwdwdwd"}<br>
+     Sunrise: ${1313113}<br>
+     Sunset: ${2323232}`
+    )
+    .openPopup();
+}
+
+loadMap();
+loadPosition();
