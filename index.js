19c3a82d51ae3542f62b81a68bf26743a4a44e63
German Eksi <german.eksi@gmail.com>
2021-05-03 01:19:47 +0300

    can display coordinates and only need to add graph to show data

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 //loading in the map
 
 //Global variables to use
-let latitude = document.getElementById("lat").value;
-let longitude = document.getElementById("long").value;
-let date = document.getElementById("date").value;
-const zoomLevel = 13;
+let form = document.querySelector(".form");
+let latitude;
+let longitude;
+let date;
 let map = L.map("map");
 let coords = [latitude, longitude];
+let api_answer;
+let zoomLevel;
+let clickedLongitude;
+let clickedLatitude;
+
+//Data for displaying when eveyrthing is submitted
+let answer_latitude = document.querySelector(".latitude");
+let answer_longitude = document.querySelector(".longitude");
 
 // Fetch sunrise, sunset and day length data from external URL.
 // For the url we need longitude, latitude. (date is optional but we will include it, so we could fetch data based on given data)
@@ -14,18 +22,59 @@ let coords = [latitude, longitude];
   let url = `https://api.sunrise-sunset.org/json?lat=${58.35833}&lng=${26.71326}&date=${today}`;
 });*/
 
-$.ajax({
-  url: `https://api.sunrise-sunset.org/json?lat=${58.35833}&lng=${26.71326}&date=${"today"}`,
-  contentType: "application/json",
-  dataType: "jsonp",
-  success: function (data) {
-    console.log(data);
-  },
+function loadAPI(latitude, longitude, date) {
+  let url;
+  if (!date) {
+    url = `https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}&date=today&formatted=1`;
+  } else {
+    date = date.toString();
+    url = `https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}&date=${date}&formatted=1`;
+  }
+
+  //Ajax request from the api
+  let api_vastus = $.ajax({
+    url: url,
+    contentType: "application/json",
+    dataType: "jsonp",
+    success: function (data) {
+      api_answer = data;
+      //console.log(api_answer);
+    },
+  });
+
+  return api_vastus;
+}
+
+//event listener for form submitting
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  latitude = document.getElementById("lat").value;
+  longitude = document.getElementById("long").value;
+  date = document.getElementById("date").value;
+  loadAPI(latitude, longitude, date);
+  loadPosition(latitude, longitude);
+  /*  function isLatitude(latitude) {
+    return isFinite(latitude) && Math.abs(latitude) <= 90;
+  }
+
+  latitude = isLatitude(latitude);
+
+  function isLongitude(longitude) {
+    return isFinite(longitude) && Math.abs(longitude) <= 180;
+  }
+
+  longitude = isLongitude(longitude);
+ */
 });
 
+// SIIA TEHA TINGIMUS, MIS LISAB MARKEREID JA ANDMEID MAPILE VASTAVALT SELLELE, KAS ON PANNAKSE LÄBI VORMI ANDMED VÕI VAJUTATAKSE KAARDILE.
+//form.addEventListener("submit", loadAPI(latitude, longitude, date));
+
 //Function for loading the map
-function loadMap() {
-  map.setView([51.505, -0.09], 13);
+function loadMap(latitude, longitude, zoomLevel) {
+  map.setView([latitude, longitude], zoomLevel);
 
   //Changed the titleLayer so it would look a bit different in style
   L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
@@ -35,26 +84,71 @@ function loadMap() {
 }
 
 //Function for loading the position
-function loadPosition() {
+function loadPosition(lat, long, date) {
   // Copied from the leafletjs original documentation page
   // Reference: https://leafletjs.com/index.html
-  L.marker([58.35833, 26.71326])
+  L.marker([lat, long])
     .addTo(map)
     .bindPopup(
       L.popup({
-        maxWidht: 250,
-        maxHeight: 150,
+        maxWidht: 700,
+        maxHeight: 200,
         className: "leaflet-popup",
+        autoPan: true,
       })
     )
     .setPopupContent(
       `
-     Day Length: ${"wdwdwdwd"}<br>
-     Sunrise: ${1313113}<br>
-     Sunset: ${2323232}`
+     Latitude:<h3>${latitude}</h3>Longitude:<h3>${longitude}</h3>Timezone:<h3 class="coord_span">UTC</h3>
+     <span class="day_length">Day Length: ${api_answer.results.day_length}</span><br>
+     <span class="api_result">Sunrise:</span> ${api_answer.results.sunrise}<br>
+     <span class="api_result">Sunset:</span> ${api_answer.results.sunset}`
     )
     .openPopup();
+
+  //binds the view straight to the given location
+  map.setView([latitude, longitude], zoomLevel);
 }
 
-loadMap();
-loadPosition();
+//Display marker and the location data on click
+function clickMap() {
+  map.on("click", function (e) {
+    latitude = e.latlng.lat;
+    longitude = e.latlng.lng;
+
+    loadAPI(latitude, longitude);
+    loadPosition(latitude, longitude);
+
+    console.log(map);
+    //For displaying the data to the client on the sidebar
+    answer_latitude.innerHTML = e.latlng.lat;
+    answer_longitude.innerHTML = e.latlng.lng;
+  });
+}
+clickMap();
+
+//Map initial load.
+loadMap(58.7523778, 25.3319078, 7);
+
+//Chart data from chart.js
+const labels = ["Katse1", "Katse2", "Katse3", "Katse5"];
+
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: "TEST LABEL",
+      backgroundColor: "black",
+      borderColor: "white",
+      data: [1, 40, 35, 21, 20],
+    },
+  ],
+};
+
+const config = {
+  type: "line",
+  data,
+  options: {},
+};
+
+var Day_Length_Chart = new Chart(document.getElementById("myChart"), config);
